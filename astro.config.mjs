1fe2178e822aae96a0c86a4fba7c5c2cc01064ff
Guus <guus.noij@gmail.com>
2024-03-18 20:30:09 +0100

    fix seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: 'https://manfredroovers.nl',
+  integrations: [
+    tailwind(), 
+    icon(), 
+    sitemap()
+  ],
   prefetch: true
 });
